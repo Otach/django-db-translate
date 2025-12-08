b18146e144d70fdaec49d2cc1e56b44b35442812
Mason Weaver <mason@swingproxy.com>
2025-12-07 21:18:57 -0500

    Remove default permissions from the empty `DBTranslatePermissions`model

--- django_db_translate/models.py
+++ django_db_translate/models.py
@@ -5,6 +5,7 @@ class DBTranslatePermissions(models.Model):
     """Unmanaged model to insert translation permissions."""
     class Meta:
         managed = False
+        default_permissions = ()
         permissions = [
             ("manage_translations", "Manage translations")
         ]
